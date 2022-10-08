93e90ff73b3386c74cdcffd4d227f1a35028ae90
LLyma <douglasllyma@hotmail.com>
2022-10-08 12:10:42 -0300

    Encremento, OperadorTernario.

---
+++ TIPOS-VARIAVEIS/Operadores.java
@@ -0,0 +1,12 @@
+public class Operadores {
+    public static void main(String[] args) {
+        String nomeCompleto = "Douglas" +" "+ "De"+" "+ "Lima";
+        System.out.println(nomeCompleto);
+
+        
+
+            
+
+
+    }
+}
---
+++ TIPOS-VARIAVEIS/Repeticao.java
@@ -0,0 +1,12 @@
+public class Repeticao {
+    public static void main(String[] args) {
+        int numero = 5;
+        // x repetição
+
+        // numero = numero + 2;
+
+        System.out.println(++numero);
+
+    }
+
+}
