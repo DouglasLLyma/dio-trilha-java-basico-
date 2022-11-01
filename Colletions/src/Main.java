edef64d856bbb409afc1f62e384501ed7316c561
LLyma <douglasllyma@hotmail.com>
2022-11-01 09:55:06 -0300

    NovoCommit

---
+++ Colletions/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+   
+    public static void main(String[] args) {
+        List<String> nomes = new ArrayList<String>();
+        nomes.add("Douglas");
+        nomes.add("Aline");
+        nomes.add("Paula");
+        nomes.add("Roberto");
+        nomes.add("João");
+        nomes.add("Karla");
+
+        System.out.println(nomes);
+    }
+}
---
+++ Set/Douglas.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Lista;
+
+public class Douglas {
+   
+    public static void main(String[] args) {
+        List<String> nomes = new ArrayList<String>();
+        nome.add("Douglas");
+        nome.add("Aline");
+        nome.add("Paula");
+
+        System.out.println(nomes);
+    }
+}
