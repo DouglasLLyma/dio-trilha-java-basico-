e792be32a608a7b704690bb32a91f3f9c6942077
LLyma <douglasllyma@hotmail.com>
2022-11-09 17:12:21 -0300

    Commit 09/11/2022

---
+++ Colletions/src/Teste.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Teste {
+    public static void main(String[] args) {
+        List<String> listaNumeros =
+        Arrays.asList("39", "45", "54", "55");
+        System.out.println(listaNumeros);
+    
+       
+    }
+}   
